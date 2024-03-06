a8f7df4535c90f3c5ab72f465ac0086ffa5c18de
李小明 <alx696@gmail.com>
2024-03-06 21:40:21 +0800

    提前查询水域名称,让离线数据中能够显示

--- layers/water_name/water_name.sql
+++ layers/water_name/water_name.sql
@@ -16,8 +16,6 @@ CREATE OR REPLACE FUNCTION layer_water_name(bbox geometry, zoom_level integer)
 AS
 $$
 SELECT
-    -- etldoc: osm_water_lakeline ->  layer_water_name:z9_13
-    -- etldoc: osm_water_lakeline ->  layer_water_name:z14_
     CASE
         WHEN osm_id < 0 THEN -osm_id * 10 + 4
         ELSE osm_id * 10 + 1
@@ -31,12 +29,9 @@ SELECT
     is_intermittent::int AS intermittent
 FROM osm_water_lakeline
 WHERE geometry && bbox
-  AND ((zoom_level BETWEEN 3 AND 13 AND LineLabel(zoom_level, NULLIF(name, ''), geometry))
-    OR (zoom_level >= 14))
+  AND zoom_level >= 10
 UNION ALL
 SELECT
-    -- etldoc: osm_water_point ->  layer_water_name:z9_13
-    -- etldoc: osm_water_point ->  layer_water_name:z14_
     CASE
         WHEN osm_id < 0 THEN -osm_id * 10 + 4
         ELSE osm_id * 10 + 1
@@ -50,17 +45,9 @@ SELECT
     is_intermittent::int AS intermittent
 FROM osm_water_point
 WHERE geometry && bbox
-  AND (
-        -- Show a label if a water feature covers at least 1/4 of a tile or z14+
-        (tags->'place' IN ('sea', 'ocean') AND POWER(4,zoom_level) * earth_area > 0.25)
-        OR (zoom_level BETWEEN 3 AND 13 AND POWER(4,zoom_level) * earth_area > 0.25)
-        OR (zoom_level >= 14)
-    )
+  AND zoom_level >= 10
 UNION ALL
 SELECT
-    -- etldoc: osm_marine_point ->  layer_water_name:z0_8
-    -- etldoc: osm_marine_point ->  layer_water_name:z9_13
-    -- etldoc: osm_marine_point ->  layer_water_name:z14_
     osm_id * 10 AS osm_id_hash,
     geometry,
     name,
@@ -74,8 +61,8 @@ WHERE geometry && bbox
   AND CASE
       WHEN place = 'ocean' THEN TRUE
       WHEN zoom_level >= "rank" AND "rank" IS NOT NULL THEN TRUE
-      WHEN "natural" = 'bay' THEN zoom_level >= 13
+      WHEN "natural" = 'bay' THEN zoom_level >= 10
       ELSE zoom_level >= 8 END;
 $$ LANGUAGE SQL STABLE
-                -- STRICT
-                PARALLEL SAFE;
+-- STRICT
+PARALLEL SAFE;
